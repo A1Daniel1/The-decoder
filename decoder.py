6a1ecbf04f35f4e5e1c1e99bdd63b36a663da4f6
Daniel Ahumada <danielahumada071@gmail.com>
2025-05-03 13:23:55 -0500

    concepto

--- decoder.py
+++ decoder.py
@@ -1,4 +1,15 @@
 def main():
-    ...
+    entrada = input("Digite a string a ser decifrada: ")
+    resultado = ""
+    for i in range(len(entrada)):
+        if entrada[i].isalpha():
+            if entrada[i].isupper():
+                resultado += chr((ord(entrada[i]) - 65 - 3) % 26 + 65)
+            else:
+                resultado += chr((ord(entrada[i]) - 97 - 3) % 26 + 97)
+        else:
+            resultado += entrada[i]
+    print("A string decifrada é:", resultado)   
 
-main()
+if __name__ == "__main__":
+    main()
